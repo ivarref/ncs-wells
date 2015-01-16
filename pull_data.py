6260931ba4983f8b972e0c3f95562f74d1cc6c56
Ivar Refsdal <refsdal.ivar@gmail.com>
2015-01-16 19:54:33 +0100

    o Get that data...

--- pull_data.py
+++ pull_data.py
@@ -59,39 +59,79 @@ def verify_and_assign(values, data):
 
 set_output_encoding()
 
-# Goal:
-# Output
-# wellbore    date    discovered_oil discovered_gas discovered_oe
-
 if not os.path.exists('data'):
   os.makedirs('data')
 
-exploration_data = [x.strip() for x in get_url('http://factpages.npd.no/ReportServer?/FactPages/TableView/wellbore_exploration_all&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.153.159&CultureCode=en', 'cache/exploration_wells.csv').split('\n') if x.strip() != '']
+# On the wellbore NPD page: Table view -> Exploration -> All - long list
+filename = 'cache/exploration_wells.csv'
+filename = 'cache/exploration_wells_all.csv'
+exploration_data = [x.strip() for x in get_url('http://factpages.npd.no/ReportServer?/FactPages/TableView/wellbore_exploration_all&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.153.159&CultureCode=en', filename).split('\n') if x.strip() != '']
 
 (wlbWellboreName,wlbWell,wlbDrillingOperator,wlbDrillingOperatorGroup,wlbProductionLicence,wlbPurpose,wlbStatus,wlbContent,wlbWellType,wlbEntryDate,wlbCompletionDate,wlbField,wlbDrillPermit,wlbDiscovery,wlbDiscoveryWellbore,wlbBottomHoleTemperature,wlbSeismicLocation,wlbMaxInclation,wlbKellyBushElevation,wlbFinalVerticalDepth,wlbTotalDepth,wlbWaterDepth,wlbAgeAtTd,wlbFormationAtTd,wlbMainArea,wlbDrillingFacility,wlbFacilityTypeDrilling,wlbLicensingActivity,wlbMultilateral,wlbPurposePlanned,wlbEntryYear,wlbCompletionYear,wlbReclassFromWellbore,wlbReentryExplorationActivity,wlbPlotSymbol,wlbFormationWithHc1,wlbAgeWithHc1,wlbFormationWithHc2,wlbAgeWithHc2,wlbFormationWithHc3,wlbAgeWithHc3,wlbDrillingDays,wlbReentry,wlbGeodeticDatum,wlbNsDeg,wlbNsMin,wlbNsSec,wlbNsCode,wlbEwDeg,wlbEwMin,wlbEwSec,wlbEwCode,wlbNsDecDeg,wlbEwDesDeg,wlbNsUtm,wlbEwUtm,wlbUtmZone,wlbNamePart1,wlbNamePart2,wlbNamePart3,wlbNamePart4,wlbNamePart5,wlbNamePart6,wlbPressReleaseUrl,wlbFactPageUrl,wlbFactMapUrl,wlbDiskosWellboreType,wlbDiskosWellboreParent,wlbWdssQcDate,wlbNpdidWellbore,dscNpdidDiscovery,fldNpdidField,fclNpdidFacilityDrilling,wlbNpdidWellboreReclass,prlNpdidProductionLicence,wlbDiskosWellOperator,wlbDateUpdated,wlbDateUpdatedMax,datesyncNPD) = verify_and_assign("wlbWellboreName,wlbWell,wlbDrillingOperator,wlbDrillingOperatorGroup,wlbProductionLicence,wlbPurpose,wlbStatus,wlbContent,wlbWellType,wlbEntryDate,wlbCompletionDate,wlbField,wlbDrillPermit,wlbDiscovery,wlbDiscoveryWellbore,wlbBottomHoleTemperature,wlbSeismicLocation,wlbMaxInclation,wlbKellyBushElevation,wlbFinalVerticalDepth,wlbTotalDepth,wlbWaterDepth,wlbAgeAtTd,wlbFormationAtTd,wlbMainArea,wlbDrillingFacility,wlbFacilityTypeDrilling,wlbLicensingActivity,wlbMultilateral,wlbPurposePlanned,wlbEntryYear,wlbCompletionYear,wlbReclassFromWellbore,wlbReentryExplorationActivity,wlbPlotSymbol,wlbFormationWithHc1,wlbAgeWithHc1,wlbFormationWithHc2,wlbAgeWithHc2,wlbFormationWithHc3,wlbAgeWithHc3,wlbDrillingDays,wlbReentry,wlbGeodeticDatum,wlbNsDeg,wlbNsMin,wlbNsSec,wlbNsCode,wlbEwDeg,wlbEwMin,wlbEwSec,wlbEwCode,wlbNsDecDeg,wlbEwDesDeg,wlbNsUtm,wlbEwUtm,wlbUtmZone,wlbNamePart1,wlbNamePart2,wlbNamePart3,wlbNamePart4,wlbNamePart5,wlbNamePart6,wlbPressReleaseUrl,wlbFactPageUrl,wlbFactMapUrl,wlbDiskosWellboreType,wlbDiskosWellboreParent,wlbWdssQcDate,wlbNpdidWellbore,dscNpdidDiscovery,fldNpdidField,fclNpdidFacilityDrilling,wlbNpdidWellboreReclass,prlNpdidProductionLicence,wlbDiskosWellOperator,wlbDateUpdated,wlbDateUpdatedMax,datesyncNPD", exploration_data)
 
 
 exploration_data = [x.split(",") for x in exploration_data[1:]]
 
-seen_discoveries = []
-
 disc = {}
 
+# Goal:
+# Output
+# wellbore    date    discovered_oil    discovered_gas    discovered_oe
+# cod-well1   1968..  123.0             0                 0
+# cod-well2   1969..  0.0               0                 0
+
+date_to_discoveries = {}
+
+field_to_type = {}
+
 for line in exploration_data:
-  discovery = line[wlbField]
-  if discovery not in disc:
-    disc[discovery] = []
   entrydate = line[wlbEntryDate].split('.')
   entrydate.reverse()
-  disc[discovery].append('-'.join(entrydate))
-
-for key in disc.keys():
-  if key.strip() == '':
+  iso8601_date = '-'.join(entrydate)
+  if iso8601_date == '':
+    # TODO 9 entries are missing this date.
+    # Let's ignore it ......
+    sys.stderr.write("[WARN] Skipping %s\n" % (str(line)))
     continue
-  disc[key].sort()
-  x = disc[key][0]
-  print "%s => %s vs %s" % (key, x, str((disc[key])))
 
+  field_to_type[line[wlbField]] = 'field'
+
+  if line[wlbField] == '':
+    line[wlbField] = line[wlbDiscovery]
+    field_to_type[line[wlbField]] = 'discovery'
+
+  if line[wlbField] == '':
+    line[wlbField] = line[wlbWellboreName]
+    field_to_type[line[wlbField]] = 'wellbore'
+
+  discovery = line[wlbField]
+  if discovery == '':
+    sys.stderr.write("This shouldn't happen.\n")
+    sys.exit(-1)
+
+  if discovery not in disc:
+    disc[discovery] = []
+  disc[discovery].append(iso8601_date)
+
+  if iso8601_date not in date_to_discoveries:
+    date_to_discoveries[iso8601_date] = []
+  date_to_discoveries[iso8601_date].append(line)
+
+print "*** Done initial parsing ..."
+
+dates = date_to_discoveries.keys()
+dates.sort()
+for date in dates:
+  wells_for_date = date_to_discoveries[date]
+  for line in wells_for_date:
+    discovery = line[wlbField]
+    reserves = { 'oil' : Decimal(0) }
+    if disc[discovery][0] == date:
+      print "numero uno '%s' => '%s'" % (date, discovery)
+      pass
+    else:
+      print "not numero uno '%s' => '%s'" % (date, discovery)
+      
 with codecs.open('data/data.tsv', encoding='utf-8', mode='w') as fd:
   fd.write('wellbore\tdate\tdiscovered_oil\n')
   fd.write('%s\n' % ('\t'.join(['1234', '1998-01-01', '123.0'])))
