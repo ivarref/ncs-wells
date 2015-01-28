03277df67ce61790a99b7dea89527123fa91b9bc
Ivar Refsdal <refsdal.ivar@gmail.com>
2015-01-28 21:35:58 +0100

    Some touched

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ Goal
 -----
 Show cumulative discoveries vs. number of cumulative wells drilled.
 
+About the data
+--------------
+The wellbores are collected from the Wellbore NPD page: Table view -> Exploration -> All - long list.
+Only wellbores with purpose 'WILDCAT' is processed further.
+
+These wells are mapped to reserves on the "Field NPD page: Table view -> Reserves"
+and "Discovery NPD page: Table view -> Resources".
+
+Data last updated 2015-01-28.
+
+
 Comments
 --------
 refsdal.ivar@gmail.com
--- pull_data.py
+++ pull_data.py
@@ -119,7 +119,8 @@ for line in exploration_data:
   date_to_discoveries[iso8601_date].append(line)
 
 def get_resources_map():
-  resources_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=2.150.32.28&CultureCode=en'
+  # On factpages: Discovery -> Resources
+  resources_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/discovery_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.153.159&CultureCode=en'
   resources = [x.strip() for x in get_url(resources_url_csv, 'cache/resources.csv').split("\n")]
   values = "dscName,dscReservesRC,dscRecoverableOil,dscRecoverableGas,dscRecoverableNGL,dscRecoverableCondensate,dscDateOffResEstDisplay,dscNpdidDiscovery,dscReservesDateUpdated,DatesyncNPD"
   if resources[0] != values:
@@ -138,7 +139,8 @@ def get_resources_map():
   return resource_map
 
 def fldname_to_reserves():
-  reserves_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.160.74&CultureCode=en'
+  # On factpages: Field -> Table View -> Reserves
+  reserves_url_csv = 'http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=84.208.153.159&CultureCode=en'
   reserves = [x.strip() for x in get_url(reserves_url_csv, 'cache/reserves.csv').split("\n") if x.strip() != ""]
   values = "fldName,fldRecoverableOil,fldRecoverableGas,fldRecoverableNGL,fldRecoverableCondensate,fldRecoverableOE,fldRemainingOil,fldRemainingGas,fldRemainingNGL,fldRemainingCondensate,fldRemainingOE,fldDateOffResEstDisplay,fldNpdidField,DatesyncNPD"
   if reserves[0] != values:
